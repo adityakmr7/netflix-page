7d907be02f9bc6c1f6a8a903614fdaec6185e889
aditya <adityakmr9672@gmail.com>
2018-10-14 15:31:57 +0530

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import bgimg from './img/bg.jpg';
+
+const Header = styled.header`
+    background: linear-gradient (
+        to right, rgba(0, 0, 0, 0.75),
+        rgba(0,0,0,0.09)
+    ),
+    url(${bgimg});
+    height: 100vh;
+    @media (max-width: 1000px) {
+        height: 90vh;
+    }
+`;
+
+const header = () => {
+    return (
+        <Header>
+            
+        </Header>
+    )
+}
+
+export default header;
